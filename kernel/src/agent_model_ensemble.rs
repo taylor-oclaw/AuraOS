d960c5d96118241a85751ccdbebea61ef6ebda02
Taylor Oclaw (ace) <taylor.oclaw@zohomail.com>
2026-03-20 14:33:31 -0400

    feat: agent_model_ensemble module (Ollama-generated, Ace)

---
+++ kernel/src/agent_model_ensemble.rs
@@ -0,0 +1,37 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+pub struct AgentModelEnsemble {
+    models: Vec<String>,
+}
+
+impl AgentModelEnsemble {
+    pub fn new() -> Self {
+        AgentModelEnsemble {
+            models: Vec::new(),
+        }
+    }
+
+    pub fn add_model(&mut self, model_name: &str) {
+        self.models.push(String::from(model_name));
+    }
+
+    pub fn remove_model(&mut self, model_name: &str) {
+        if let Some(index) = self.models.iter().position(|m| m == model_name) {
+            self.models.remove(index);
+        }
+    }
+
+    pub fn list_models(&self) -> Vec<String> {
+        self.models.clone()
+    }
+
+    pub fn has_model(&self, model_name: &str) -> bool {
+        self.models.contains(&String::from(model_name))
+    }
+
+    pub fn clear_models(&mut self) {
+        self.models.clear();
+    }
+}
