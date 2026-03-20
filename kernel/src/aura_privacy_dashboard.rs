4d3646c051b633bbfca8431830d2114d1e2344ce
Taylor Oclaw (sophie) <taylor.oclaw@zohomail.com>
2026-03-20 16:39:56 -0400

    feat: aura_privacy_dashboard module (Ollama-generated, sophie)

---
+++ kernel/src/aura_privacy_dashboard.rs
@@ -0,0 +1,52 @@
+extern crate alloc;
+
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[no_mangle]
+pub extern "C" fn rust_ffi_init() {
+    // Initialize the module
+}
+
+#[no_mangle]
+pub extern "C" fn rust_ffi_exit() {
+    // Clean up the module
+}
+
+pub struct AuraPrivacyDashboard {
+    user_data: Vec<String>,
+    privacy_settings: Vec<bool>,
+}
+
+impl AuraPrivacyDashboard {
+    pub fn new() -> Self {
+        AuraPrivacyDashboard {
+            user_data: Vec::new(),
+            privacy_settings: vec![false; 5],
+        }
+    }
+
+    pub fn add_user_data(&mut self, data: String) {
+        self.user_data.push(data);
+    }
+
+    pub fn get_user_data(&self) -> &Vec<String> {
+        &self.user_data
+    }
+
+    pub fn set_privacy_setting(&mut self, index: usize, setting: bool) {
+        if index < self.privacy_settings.len() {
+            self.privacy_settings[index] = setting;
+        }
+    }
+
+    pub fn get_privacy_setting(&self, index: usize) -> Option<bool> {
+        self.privacy_settings.get(index).cloned()
+    }
+
+    pub fn toggle_privacy_setting(&mut self, index: usize) {
+        if let Some(setting) = self.privacy_settings.get_mut(index) {
+            *setting = !*setting;
+        }
+    }
+}
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,6 +5,7 @@
 extern crate alloc;
 
 
+mod aura_privacy_dashboard;
 use bootloader_api::{entry_point, BootInfo, BootloaderConfig};
 use core::panic::PanicInfo;
 
