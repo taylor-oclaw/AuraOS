1492aa004d54bb44b5f0967f97f858e13550b18b
Taylor Oclaw (priya) <taylor.oclaw@zohomail.com>
2026-03-20 17:24:53 -0400

    feat: triton_compat module (Ollama-generated, priya)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,6 +5,7 @@
 extern crate alloc;
 
 
+mod triton_compat;
 use bootloader_api::{entry_point, BootInfo, BootloaderConfig};
 use core::panic::PanicInfo;
 
---
+++ kernel/src/triton_compat.rs
@@ -0,0 +1,47 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[no_mangle]
+pub extern "C" fn triton_compat_init() -> i32 {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn triton_compat_exit() -> i32 {
+    0
+}
+
+pub struct TritonCompat {
+    data: Vec<u8>,
+    name: String,
+}
+
+impl TritonCompat {
+    pub fn new(name: &str) -> Self {
+        TritonCompat {
+            data: Vec::new(),
+            name: String::from(name),
+        }
+    }
+
+    pub fn add_data(&mut self, byte: u8) {
+        self.data.push(byte);
+    }
+
+    pub fn get_data(&self) -> &[u8] {
+        &self.data
+    }
+
+    pub fn clear_data(&mut self) {
+        self.data.clear();
+    }
+
+    pub fn set_name(&mut self, name: &str) {
+        self.name = String::from(name);
+    }
+
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+}
