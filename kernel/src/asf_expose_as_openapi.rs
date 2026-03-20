cdcee2fc5af5d7b96ca8991d90b9215871d0d27d
Taylor Oclaw (james) <taylor.oclaw@zohomail.com>
2026-03-20 19:44:11 -0400

    feat: asf_expose_as_openapi module (Ollama-generated, James)

---
+++ kernel/src/asf_expose_as_openapi.rs
@@ -0,0 +1,61 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[no_mangle]
+pub extern "C" fn asf_expose_as_openapi_init() {
+    // Initialization logic for the module
+}
+
+#[no_mangle]
+pub extern "C" fn asf_expose_as_openapi_exit() {
+    // Cleanup logic for the module
+}
+
+pub struct OpenAPIHandler {
+    endpoints: Vec<String>,
+    data: Vec<u8>,
+}
+
+impl OpenAPIHandler {
+    pub fn new() -> Self {
+        OpenAPIHandler {
+            endpoints: Vec::new(),
+            data: Vec::new(),
+        }
+    }
+
+    pub fn add_endpoint(&mut self, endpoint: &str) {
+        self.endpoints.push(endpoint.to_string());
+    }
+
+    pub fn get_endpoints(&self) -> &[String] {
+        &self.endpoints
+    }
+
+    pub fn set_data(&mut self, data: Vec<u8>) {
+        self.data = data;
+    }
+
+    pub fn get_data(&self) -> &[u8] {
+        &self.data
+    }
+
+    pub fn handle_request(&self, endpoint: &str) -> Option<&[u8]> {
+        if self.endpoints.contains(&endpoint.to_string()) {
+            Some(&self.data)
+        } else {
+            None
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn asf_expose_as_openapi_create_handler() -> *mut OpenAPIHandler {
+    Box::into_raw(Box::new(OpenAPIHandler::new()))
+}
+
+#[no_mangle]
+pub extern "C" fn asf_expose_as_openapi_destroy_handler(handler: *mut OpenAPIHandler) {
+    unsafe { drop(Box::from_raw(handler)) }
+}
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,6 +5,7 @@
 extern crate alloc;
 
 
+mod asf_expose_as_openapi;
 use bootloader_api::{entry_point, BootInfo, BootloaderConfig};
 use core::panic::PanicInfo;
 
