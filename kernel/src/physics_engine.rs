c7a37b2598ae59d35849e426bbcb74dd53b79a88
Taylor Oclaw (james) <taylor.oclaw@zohomail.com>
2026-03-20 16:48:11 -0400

    feat: physics_engine module (Ollama-generated, James)

---
+++ kernel/src/physics_engine.rs
@@ -0,0 +1,98 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[no_mangle]
+pub extern "C" fn rust_physics_engine_init() {
+    println!("Physics engine initialized!");
+}
+
+#[no_mangle]
+pub extern "C" fn rust_physics_engine_exit() {
+    println!("Physics engine exited.");
+}
+
+pub struct PhysicsEngine {
+    objects: Vec<Object>,
+}
+
+impl PhysicsEngine {
+    pub fn new() -> Self {
+        PhysicsEngine { objects: Vec::new() }
+    }
+
+    pub fn add_object(&mut self, object: Object) {
+        self.objects.push(object);
+    }
+
+    pub fn remove_object(&mut self, index: usize) -> Option<Object> {
+        if index < self.objects.len() {
+            Some(self.objects.remove(index))
+        } else {
+            None
+        }
+    }
+
+    pub fn update_positions(&mut self, delta_time: f32) {
+        for object in &mut self.objects {
+            object.position.x += object.velocity.x * delta_time;
+            object.position.y += object.velocity.y * delta_time;
+        }
+    }
+
+    pub fn get_objects(&self) -> &[Object] {
+        &self.objects
+    }
+}
+
+pub struct Object {
+    name: String,
+    position: Vector2,
+    velocity: Vector2,
+}
+
+impl Object {
+    pub fn new(name: String, position: Vector2, velocity: Vector2) -> Self {
+        Object { name, position, velocity }
+    }
+
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn set_velocity(&mut self, velocity: Vector2) {
+        self.velocity = velocity;
+    }
+}
+
+pub struct Vector2 {
+    x: f32,
+    y: f32,
+}
+
+impl Vector2 {
+    pub fn new(x: f32, y: f32) -> Self {
+        Vector2 { x, y }
+    }
+
+    pub fn add(&self, other: &Vector2) -> Vector2 {
+        Vector2 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+
+    pub fn subtract(&self, other: &Vector2) -> Vector2 {
+        Vector2 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+
+    pub fn scale(&self, scalar: f32) -> Vector2 {
+        Vector2 {
+            x: self.x * scalar,
+            y: self.y * scalar,
+        }
+    }
+}
