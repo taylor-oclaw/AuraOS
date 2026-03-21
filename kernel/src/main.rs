cf815c4fd133956b8021721894259b22f609af48
Taylor Oclaw (james) <taylor.oclaw@zohomail.com>
2026-03-21 14:01:51 -0400

    feat: rel_holiday_greet module (Ollama-generated, James)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,6 +5,7 @@
 extern crate alloc;
 
 
+mod rel_holiday_greet;
 use bootloader_api::{entry_point, BootInfo, BootloaderConfig};
 use core::panic::PanicInfo;
 
---
+++ kernel/src/rel_holiday_greet.rs
@@ -0,0 +1,49 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[no_mangle]
+pub extern "C" fn rel_holiday_greet_init() {
+    // Initialization logic for the module
+}
+
+#[no_mangle]
+pub extern "C" fn rel_holiday_greet_exit() {
+    // Cleanup logic for the module
+}
+
+pub struct HolidayGreeting {
+    greetings: Vec<String>,
+}
+
+impl HolidayGreeting {
+    pub fn new() -> Self {
+        HolidayGreeting {
+            greetings: Vec::new(),
+        }
+    }
+
+    pub fn add_greeting(&mut self, greeting: String) {
+        self.greetings.push(greeting);
+    }
+
+    pub fn get_greeting(&self, index: usize) -> Option<&String> {
+        self.greetings.get(index)
+    }
+
+    pub fn remove_greeting(&mut self, index: usize) -> Option<String> {
+        if index < self.greetings.len() {
+            Some(self.greetings.remove(index))
+        } else {
+            None
+        }
+    }
+
+    pub fn list_greetings(&self) -> &[String] {
+        &self.greetings
+    }
+
+    pub fn count_greetings(&self) -> usize {
+        self.greetings.len()
+    }
+}
