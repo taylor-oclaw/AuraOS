89692917e15725d830e141e15dff0fedff0739f4
Taylor Oclaw <taylor.oclaw@zohomail.com>
2026-03-21 18:36:20 -0400

    auto-merge: 2 new modules

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -508,6 +508,8 @@ entry_point!(kernel_main, config = &BOOTLOADER_CONFIG);
 
 
 
+
+
 mod a2a_artifact_handler;
 mod a2a_host;
 mod a2a_protocol;
@@ -2489,6 +2491,11 @@ mod people_seasonal_context;
 mod people_sensitive_topic;
 mod people_shared_memory;
 mod people_social_extract;
+mod people_source_email_cc;
+mod people_source_email_sent;
+mod people_source_imessage;
+mod people_source_signal;
+mod people_source_whatsapp;
 mod people_story_recall;
 mod people_support_suggest;
 mod people_timeline_build;
