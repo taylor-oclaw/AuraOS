c5bf721c46770c5a91bcbf6a037cc5c0fdac12fb
Taylor Oclaw (ace) <taylor.oclaw@zohomail.com>
2026-03-21 01:48:39 -0400

    feat: lang_core module (Ollama-generated, Ace)

---
+++ kernel/src/lang_core.rs
@@ -0,0 +1,47 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[no_mangle]
+pub extern "C" fn lang_core_init() {
+    // Initialization logic for the module
+}
+
+#[no_mangle]
+pub extern "C" fn lang_core_exit() {
+    // Cleanup logic for the module
+}
+
+pub struct LangCore {
+    data: Vec<u8>,
+    name: String,
+}
+
+impl LangCore {
+    pub fn new(name: &str) -> Self {
+        LangCore {
+            data: Vec::new(),
+            name: String::from(name),
+        }
+    }
+
+    pub fn add_data(&mut self, byte: u8) {
+        self.data.push(byte);
+    }
+
+    pub fn get_data(&self) -> &[u8] {
+        &self.data
+    }
+
+    pub fn clear_data(&mut self) {
+        self.data.clear();
+    }
+
+    pub fn set_name(&mut self, new_name: &str) {
+        self.name = String::from(new_name);
+    }
+
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+}
