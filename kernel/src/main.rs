d87b0964b0c05c42bd19b66e8c23d7f9497ea33b
Taylor Oclaw (marcus) <taylor.oclaw@zohomail.com>
2026-03-21 15:20:03 -0400

    feat: people_never_mention module (Ollama-generated, marcus)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,6 +5,7 @@
 extern crate alloc;
 
 
+mod people_never_mention;
 use bootloader_api::{entry_point, BootInfo, BootloaderConfig};
 use core::panic::PanicInfo;
 
---
+++ kernel/src/people_never_mention.rs
@@ -0,0 +1,53 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[no_mangle]
+pub extern "C" fn init_module() -> i32 {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn cleanup_module() -> i32 {
+    0
+}
+
+pub struct PeopleNeverMention {
+    names: Vec<String>,
+}
+
+impl PeopleNeverMention {
+    pub fn new() -> Self {
+        PeopleNeverMention {
+            names: Vec::new(),
+        }
+    }
+
+    pub fn add_name(&mut self, name: String) {
+        if !self.names.contains(&name) {
+            self.names.push(name);
+        }
+    }
+
+    pub fn remove_name(&mut self, name: &str) -> bool {
+        let pos = self.names.iter().position(|n| n == name);
+        if let Some(index) = pos {
+            self.names.remove(index);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn get_names(&self) -> Vec<String> {
+        self.names.clone()
+    }
+
+    pub fn contains_name(&self, name: &str) -> bool {
+        self.names.contains(&String::from(name))
+    }
+
+    pub fn clear_names(&mut self) {
+        self.names.clear();
+    }
+}
