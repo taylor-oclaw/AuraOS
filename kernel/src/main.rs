f9a30b9ad010f95a7e668409cc246bb54b4e8c00
Taylor Oclaw (marcus) <taylor.oclaw@zohomail.com>
2026-03-21 17:44:42 -0400

    feat: profile_wfh_meeting_auto_switch module (Ollama-generated, marcus)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,6 +5,7 @@
 extern crate alloc;
 
 
+mod profile_wfh_meeting_auto_switch;
 use bootloader_api::{entry_point, BootInfo, BootloaderConfig};
 use core::panic::PanicInfo;
 
---
+++ kernel/src/profile_wfh_meeting_auto_switch.rs
@@ -0,0 +1,59 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[no_mangle]
+pub extern "C" fn rust_start() -> ! {
+    // Entry point for the kernel module
+    let mut profile = ProfileWFHFocusBlockWork::new();
+    profile.add_focus_area("Coding");
+    profile.add_focus_area("Reading");
+    profile.log_activity("Started coding session");
+    profile.log_activity("Read a chapter of a book");
+    profile.update_focus_time(30);
+    profile.print_summary();
+
+    loop {}
+}
+
+pub struct ProfileWFHFocusBlockWork {
+    focus_areas: Vec<String>,
+    activities_log: Vec<String>,
+    total_focus_time: u32,
+}
+
+impl ProfileWFHFocusBlockWork {
+    pub fn new() -> Self {
+        ProfileWFHFocusBlockWork {
+            focus_areas: Vec::new(),
+            activities_log: Vec::new(),
+            total_focus_time: 0,
+        }
+    }
+
+    pub fn add_focus_area(&mut self, area: &str) {
+        self.focus_areas.push(String::from(area));
+    }
+
+    pub fn log_activity(&mut self, activity: &str) {
+        self.activities_log.push(String::from(activity));
+    }
+
+    pub fn update_focus_time(&mut self, minutes: u32) {
+        self.total_focus_time += minutes;
+    }
+
+    pub fn get_focus_areas(&self) -> &[String] {
+        &self.focus_areas
+    }
+
+    pub fn print_summary(&self) {
+        for area in &self.focus_areas {
+            println!("Focus Area: {}", area);
+        }
+        for activity in &self.activities_log {
+            println!("Activity: {}", activity);
+        }
+        println!("Total Focus Time: {} minutes", self.total_focus_time);
+    }
+}
