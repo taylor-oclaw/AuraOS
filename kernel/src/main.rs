ba1c49ee67621626f793016093cef77c53e2df72
Taylor Oclaw (priya) <taylor.oclaw@zohomail.com>
2026-03-21 06:19:14 -0400

    feat: offline_contact_cache module (Ollama-generated, priya)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,6 +5,7 @@
 extern crate alloc;
 
 
+mod offline_contact_cache;
 use bootloader_api::{entry_point, BootInfo, BootloaderConfig};
 use core::panic::PanicInfo;
 
---
+++ kernel/src/offline_contact_cache.rs
@@ -0,0 +1,51 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+pub struct OfflineContactCache {
+    contacts: Vec<Contact>,
+}
+
+impl OfflineContactCache {
+    pub fn new() -> Self {
+        OfflineContactCache {
+            contacts: Vec::new(),
+        }
+    }
+
+    pub fn add_contact(&mut self, name: String, phone_number: String) {
+        let contact = Contact { name, phone_number };
+        self.contacts.push(contact);
+    }
+
+    pub fn remove_contact_by_name(&mut self, name: &str) -> bool {
+        if let Some(index) = self.contacts.iter().position(|c| c.name == name) {
+            self.contacts.remove(index);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn get_contact_by_name(&self, name: &str) -> Option<&Contact> {
+        self.contacts.iter().find(|c| c.name == name)
+    }
+
+    pub fn list_contacts(&self) -> Vec<String> {
+        self.contacts.iter().map(|c| format!("{}: {}", c.name, c.phone_number)).collect()
+    }
+
+    pub fn update_contact_phone_number(&mut self, name: &str, new_phone_number: String) -> bool {
+        if let Some(contact) = self.contacts.iter_mut().find(|c| c.name == name) {
+            contact.phone_number = new_phone_number;
+            true
+        } else {
+            false
+        }
+    }
+}
+
+struct Contact {
+    name: String,
+    phone_number: String,
+}
