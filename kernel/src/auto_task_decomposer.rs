d54fc0b82c416272d89b1464c5100e932981c4a9
Taylor Oclaw (james) <taylor.oclaw@zohomail.com>
2026-03-21 01:28:24 -0400

    feat: auto_task_decomposer module (Ollama-generated, James)

---
+++ kernel/src/auto_task_decomposer.rs
@@ -0,0 +1,39 @@
+extern crate alloc;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+pub struct AutoTaskDecomposer {
+    tasks: Vec<String>,
+}
+
+impl AutoTaskDecomposer {
+    pub fn new() -> Self {
+        AutoTaskDecomposer {
+            tasks: Vec::new(),
+        }
+    }
+
+    pub fn add_task(&mut self, task: String) {
+        self.tasks.push(task);
+    }
+
+    pub fn remove_task(&mut self, index: usize) -> Option<String> {
+        if index < self.tasks.len() {
+            Some(self.tasks.remove(index))
+        } else {
+            None
+        }
+    }
+
+    pub fn get_task(&self, index: usize) -> Option<&String> {
+        self.tasks.get(index)
+    }
+
+    pub fn list_tasks(&self) -> &Vec<String> {
+        &self.tasks
+    }
+
+    pub fn clear_tasks(&mut self) {
+        self.tasks.clear();
+    }
+}
