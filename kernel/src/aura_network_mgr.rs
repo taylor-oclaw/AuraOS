99741d9fc5d38553acb48a93107f66746fc15bfa
Taylor Oclaw (priya) <taylor.oclaw@zohomail.com>
2026-03-22 00:00:58 -0400

    feat: aura_network_mgr module (rebuilt, priya)

--- kernel/src/aura_network_mgr.rs
+++ kernel/src/aura_network_mgr.rs
@@ -3,59 +3,48 @@ use alloc::string::String;
 use alloc::vec::Vec;
 
 pub struct AuraNetworkMgr {
-    interfaces: Vec<String>,
-    connected_peers: Vec<String>,
-    network_config: String,
+    network_interfaces: Vec<String>,
 }
 
 impl AuraNetworkMgr {
     pub fn new() -> Self {
         AuraNetworkMgr {
-            interfaces: Vec::new(),
-            connected_peers: Vec::new(),
-            network_config: String::from("default"),
+            network_interfaces: Vec::<String>::new(),
         }
     }
 
-    pub fn add_interface(&mut self, interface_name: &str) {
-        self.interfaces.push(String::from(interface_name));
+    pub fn add_network_interface(&mut self, interface_name: String) {
+        self.network_interfaces.push(interface_name);
     }
 
-    pub fn remove_interface(&mut self, interface_name: &str) -> bool {
-        if let Some(index) = self.interfaces.iter().position(|x| x == interface_name) {
-            self.interfaces.remove(index);
-            true
-        } else {
-            false
-        }
+    pub fn remove_network_interface(&mut self, interface_name: &str) {
+        let index = self
+            .network_interfaces
+            .iter()
+            .position(|x| x == interface_name)
+            .unwrap_or_else(|| panic!("Interface not found"));
+        self.network_interfaces.remove(index);
     }
 
-    pub fn list_interfaces(&self) -> Vec<String> {
-        self.interfaces.clone()
+    pub fn get_network_interfaces(&self) -> Vec<String> {
+        self.network_interfaces.clone()
     }
 
-    pub fn connect_to_peer(&mut self, peer_address: &str) {
-        self.connected_peers.push(String::from(peer_address));
+    pub fn is_interface_up(&self, interface_name: &str) -> bool {
+        let index = self
+            .network_interfaces
+            .iter()
+            .position(|x| x == interface_name)
+            .unwrap_or_else(|| panic!("Interface not found"));
+        self.network_interfaces[index].len() > 0
     }
 
-    pub fn disconnect_from_peer(&mut self, peer_address: &str) -> bool {
-        if let Some(index) = self.connected_peers.iter().position(|x| x == peer_address) {
-            self.connected_peers.remove(index);
-            true
-        } else {
-            false
-        }
-    }
-
-    pub fn list_connected_peers(&self) -> Vec<String> {
-        self.connected_peers.clone()
-    }
-
-    pub fn set_network_config(&mut self, config: &str) {
-        self.network_config = String::from(config);
-    }
-
-    pub fn get_network_config(&self) -> String {
-        self.network_config.clone()
+    pub fn get_interface_status(&self, interface_name: &str) -> String {
+        let index = self
+            .network_interfaces
+            .iter()
+            .position(|x| x == interface_name)
+            .unwrap_or_else(|| panic!("Interface not found"));
+        format!("{} is up", self.network_interfaces[index])
     }
 }
