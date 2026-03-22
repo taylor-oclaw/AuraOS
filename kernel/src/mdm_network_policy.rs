98b49071946f32469dc64de2b8cd88d8d2eaa97e
Taylor Oclaw (priya) <taylor.oclaw@zohomail.com>
2026-03-22 03:58:12 -0400

    feat: mdm_network_policy module (rebuilt, priya)

--- kernel/src/mdm_network_policy.rs
+++ kernel/src/mdm_network_policy.rs
@@ -2,82 +2,75 @@ extern crate alloc;
 use alloc::string::String;
 use alloc::vec::Vec;
 
-#[repr(C)]
-pub struct NetworkPolicy {
-    rules: Vec<Rule>,
+pub struct MdmNetworkPolicy {
+    policy_id: u32,
+    network_type: String,
+    ip_address: String,
 }
 
-impl NetworkPolicy {
-    pub fn new() -> Self {
-        NetworkPolicy { rules: Vec::new() }
-    }
-
-    pub fn add_rule(&mut self, rule: Rule) {
-        self.rules.push(rule);
-    }
-
-    pub fn remove_rule(&mut self, index: usize) -> Option<Rule> {
-        if index < self.rules.len() {
-            Some(self.rules.remove(index))
-        } else {
-            None
+impl MdmNetworkPolicy {
+    pub fn new(policy_id: u32, network_type: &str, ip_address: &str) -> Self {
+        MdmNetworkPolicy {
+            policy_id,
+            network_type: String::from(network_type),
+            ip_address: String::from(ip_address),
         }
     }
 
-    pub fn get_rules(&self) -> &Vec<Rule> {
-        &self.rules
+    pub fn get_policy_id(&self) -> u32 {
+        self.policy_id
     }
 
-    pub fn is_allowed(&self, packet: &Packet) -> bool {
-        for rule in &self.rules {
-            if rule.matches(packet) {
-                return rule.action == Action::Allow;
+    pub fn set_network_type(&mut self, new_type: &str) {
+        self.network_type = String::from(new_type);
+    }
+
+    pub fn get_network_type(&self) -> &str {
+        &self.network_type
+    }
+
+    pub fn update_ip_address(&mut self, new_ip: &str) {
+        self.ip_address = String::from(new_ip);
+    }
+
+    pub fn get_ip_address(&self) -> &str {
+        &self.ip_address
+    }
+}
+
+pub struct MdmNetworkPolicyList {
+    policies: Vec<MdmNetworkPolicy>,
+}
+
+impl MdmNetworkPolicyList {
+    pub fn new() -> Self {
+        MdmNetworkPolicyList { policies: Vec::new() }
+    }
+
+    pub fn add_policy(&mut self, policy: MdmNetworkPolicy) {
+        self.policies.push(policy);
+    }
+
+    pub fn get_policies(&self) -> &Vec<MdmNetworkPolicy> {
+        &self.policies
+    }
+
+    pub fn remove_policy(&mut self, policy_id: u32) -> bool {
+        for (i, policy) in self.policies.iter().enumerate() {
+            if policy.get_policy_id() == policy_id {
+                self.policies.remove(i);
+                return true;
             }
         }
         false
     }
 
-    pub fn update_rule(&mut self, index: usize, new_rule: Rule) -> Option<Rule> {
-        if index < self.rules.len() {
-            Some(core::mem::replace(&mut self.rules[index], new_rule))
-        } else {
-            None
+    pub fn get_policy(&self, policy_id: u32) -> Option<&MdmNetworkPolicy> {
+        for policy in &self.policies {
+            if policy.get_policy_id() == policy_id {
+                return Some(policy);
+            }
         }
-    }
-}
-
-#[derive(Clone)]
-pub struct Rule {
-    src_ip: String,
-    dst_ip: String,
-    action: Action,
-}
-
-impl Rule {
-    pub fn new(src_ip: String, dst_ip: String, action: Action) -> Self {
-        Rule { src_ip, dst_ip, action }
-    }
-
-    pub fn matches(&self, packet: &Packet) -> bool {
-        self.src_ip == packet.src_ip && self.dst_ip == packet.dst_ip
-    }
-}
-
-#[derive(Clone)]
-pub enum Action {
-    Allow,
-    Deny,
-}
-
-#[repr(C)]
-pub struct Packet {
-    src_ip: String,
-    dst_ip: String,
-    // Add other fields as necessary
-}
-
-impl Packet {
-    pub fn new(src_ip: String, dst_ip: String) -> Self {
-        Packet { src_ip, dst_ip }
+        None
     }
 }
