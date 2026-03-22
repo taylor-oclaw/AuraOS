b2ab0917b58b34f233c3fae8c7f4df924aba3a98
Taylor Oclaw (priya) <taylor.oclaw@zohomail.com>
2026-03-22 01:19:04 -0400

    feat: container_mkv module (rebuilt, priya)

--- kernel/src/container_mkv.rs
+++ kernel/src/container_mkv.rs
@@ -2,72 +2,30 @@ extern crate alloc;
 use alloc::string::String;
 use alloc::vec::Vec;
 
-#[no_mangle]
-pub extern "C" fn container_mkv_init() {
-    // Initialization logic for the module
-}
-
-#[no_mangle]
-pub extern "C" fn container_mkv_exit() {
-    // Cleanup logic for the module
-}
-
 pub struct ContainerMKV {
-    items: Vec<String>,
+    data: Vec<u8>,
 }
 
 impl ContainerMKV {
     pub fn new() -> Self {
-        ContainerMKV {
-            items: Vec::new(),
+        ContainerMKV { data: Vec::new() }
+    }
+
+    pub fn add_data(&mut self, data: &[u8]) {
+        self.data.extend_from_slice(data);
+    }
+
+    pub fn get_data(&self) -> &[u8] {
+        &self.data
+    }
+
+    pub fn remove_data(&mut self, start: usize, len: usize) {
+        if start + len <= self.data.len() {
+            self.data = self.data.split_at_mut(start).1.split_at(len).0;
         }
     }
 
-    pub fn add_item(&mut self, item: String) {
-        self.items.push(item);
-    }
-
-    pub fn remove_item(&mut self, index: usize) -> Option<String> {
-        if index < self.items.len() {
-            Some(self.items.remove(index))
-        } else {
-            None
-        }
-    }
-
-    pub fn get_item(&self, index: usize) -> Option<&String> {
-        self.items.get(index)
-    }
-
-    pub fn get_all_items(&self) -> &Vec<String> {
-        &self.items
-    }
-
-    pub fn clear(&mut self) {
-        self.items.clear();
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_container_mkv() {
-        let mut container = ContainerMKV::new();
-        assert_eq!(container.get_all_items().len(), 0);
-
-        container.add_item(String::from("item1"));
-        container.add_item(String::from("item2"));
-        assert_eq!(container.get_all_items().len(), 2);
-        assert_eq!(container.get_item(0), Some(&String::from("item1")));
-        assert_eq!(container.get_item(1), Some(&String::from("item2")));
-
-        let removed = container.remove_item(0);
-        assert_eq!(removed, Some(String::from("item1")));
-        assert_eq!(container.get_all_items().len(), 1);
-
-        container.clear();
-        assert_eq!(container.get_all_items().len(), 0);
+    pub fn get_size(&self) -> usize {
+        self.data.len()
     }
 }
